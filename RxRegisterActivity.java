0c9f5ae85e2569cd5283a01293f580ecd5a21fa8
brjwebb <brjwebb@gmail.com>
2017-02-19 06:07:32 -0500

    Added Activity and Layout
    Contains the form for the Rx Register Activities. First Draft.

---
+++ RxRegisterActivity.java
@@ -0,0 +1,12 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Bradford Webb on 2/19/2017.
+ */
+public class RxRegisterActivity.java extends Activity{
+@Override
+public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        }
+        }
